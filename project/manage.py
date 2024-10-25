65124f45852571b4d341bd21f915cc9628c7bb5d
SlavaB3BROU <138304199+SlavaB3BROU@users.noreply.github.com>
2024-10-25 19:56:46 +0300

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-79812364897612349687124
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
@@ -20,4 +20,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-xddddddddd
